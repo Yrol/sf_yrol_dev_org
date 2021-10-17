75612bc740dbf90cf002f369c1cf16df43f899ec
Yrol <yrollgayanth@yahoo.com>
2021-10-17 20:56:27 +1100

    Adding the event handlers

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
+    startGame : function(component, event, helper) {
+        console.log("Game button Clicked!");
+    },
 
+    reshuffleBoard : function(component, event, helper) {
+        console.log("Board reshuffle!");
     }
 })
