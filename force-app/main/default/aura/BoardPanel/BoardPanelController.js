90e6fa1811007c5f35c90a8907c14cabbe0de5e8
Yrol <yrollgayanth@yahoo.com>
2021-10-17 22:52:27 +1100

    Setting the aura ID for the combobox

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,6 +1,13 @@
 ({
     startGame : function(component, event, helper) {
-        console.log("Game button Clicked!");
+
+        //Accessing the combo box component
+        let gameModeComboBox =  component.find("gameMode");
+
+        //accessing the selected value of the combo box
+        let selectedValue = gameModeComboBox.get("v.value");
+
+        console.log(selectedValue);
     },
 
     reshuffleBoard : function(component, event, helper) {
