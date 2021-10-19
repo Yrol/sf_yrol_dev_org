e582407b36e79a0482ab1557b3e56cd2da43fd27
Yrol <yrollgayanth@yahoo.com>
2021-10-19 21:54:39 +1100

    Using the event handlers and adding the randomize function to the helper class

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(component, event, helper) {
+        console.log(helper.getWords(6));
+    },
 
+    doRender : function(component, event, helper) {
+        console.log("Render completed");
     }
 })
