eb6cc052bbe1ac6af3af94c91d3a69aad8cb029a
Yrol <yrollgayanth@yahoo.com>
2021-10-21 07:46:18 +1100

    Setting the game mode and block size

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,7 +1,27 @@
 ({
     doInit : function(component, event, helper) {
+
+        //getting the current game mode
+        const gameMode = component.get('v.gameMode');
+
+        let columns = 0;
+
+        //determining the number of columns
+        if(gameMode && gameMode === 'medium'){
+            columns = 4;
+        } else if(gameMode && gameMode === 'hard'){
+            columns = 6;
+        } else {
+            columns = 3;
+        }
+
+        //getting the grid size (12 is the horizontal number of maximum columns in Aura framework)
+        let blockSize = 12/columns;
+        console.log('block size: ' + blockSize)
+        component.set('v.blockSize', blockSize);
+
         //getting the random words
-        let words = helper.getWords(6)
+        let words = helper.getWords(columns * columns)
         console.log(words);
 
         //getting the win word
